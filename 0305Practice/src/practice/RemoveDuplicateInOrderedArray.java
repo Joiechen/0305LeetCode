9753753d1076a034ff43a1335fa4a7cb909b754c
joiechen <joie.chen@vechain.com>
2020-03-06 13:32:55 +0800

    day1

---
+++ 0305Practice/src/practice/RemoveDuplicateInOrderedArray.java
@@ -0,0 +1,62 @@
+package practice;
+
+public class RemoveDuplicateInOrderedArray {
+
+  /**
+   * 快慢双指针，时间复杂度O(n),空间复杂度O(1)
+   * @param nums
+   * @return
+   */
+  public static int removeDuplicates01(int[] nums) {
+    if(nums.length==0)
+    {
+       return 0;
+    }
+
+    int i = 0;
+      for(int j=1;j<nums.length;j++){
+        if(nums[j]!=nums[i]){
+          i++;
+          nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+  }
+
+  /**
+   * 遍历数组，空间复杂度O(1),时间复杂度O(n)
+   */
+  public static int removeDuplicates02(int[] nums) {
+    if(nums.length==0)
+    {
+       return 0;
+    }
+
+    int count = 1;
+    int index = 0;
+    for(int i = 0;i<nums.length-1;i++)
+    {
+      if(nums[i]==nums[i+1]){
+        continue;
+      } else {
+        index++;
+        nums[index] = nums[i+1];
+        count++;
+      }
+    }
+    return count;
+  }
+    
+    public static void main(String[] args) throws Exception {
+      int[] testNums01 = {1,1,2,3};
+      int[] testNums02 = {-1,1,2,3};
+      int[] testNums03 = {-1,1,2,3,3,5};
+      System.out.println(removeDuplicates01(testNums01));
+      System.out.println(removeDuplicates01(testNums02));
+      System.out.println(removeDuplicates01(testNums03));
+
+      System.out.println(removeDuplicates02(testNums01));
+      System.out.println(removeDuplicates02(testNums02));
+      System.out.println(removeDuplicates02(testNums03));
+    }
+}
