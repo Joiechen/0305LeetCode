89f36774eb53dbeb7c1fd698a0cfbaa0ed795237
joiechen <joie.chen@vechain.com>
2020-03-06 15:21:32 +0800

    day2 method2

--- 0305Practice/src/practice/RotateArray.java
+++ 0305Practice/src/practice/RotateArray.java
@@ -3,7 +3,7 @@ package practice;
 public class RotateArray{
 
   /***
-   * 旋转K次，时间复杂度N(k*n),空间复杂度O(1)
+   * 旋转K次，时间复杂度O(k*n),空间复杂度O(1)
    * @param nums
    * @param k
    */
@@ -20,11 +20,36 @@ public class RotateArray{
     return nums;
   }
 
+  /***
+   * 环状替换，时间复杂度O(n),空间复杂度O(1)
+   * @param nums
+   * @param k
+   */
+  public static int[] rotate02(int[] nums, int k){
+    k = k % nums.length;
+    int count = 0;
+    for(int start = 0; count < nums.length; start++){
+      int current = start;
+      int prev = nums[start];
+      do{
+        int next = (current + k) % nums.length;
+        int temp = nums[next];
+        nums[next] = prev;
+        prev = temp;
+        current = next;
+        count++;
+      } while (start != current);
+    }
+    return nums;
+  }
+
   public static void main(String[] args) throws Exception {
     int[] testnums1 = {9,8,7,6,5,3,4,3,2,1};
     int[] res1 = rotate01(testnums1,3);
+    int[] res2 = rotate02(testnums1,3);
     for(int i=0;i<testnums1.length;i++){
         System.out.println(res1[i]);
+        System.out.println(res2[i]);
     }
   
   }
