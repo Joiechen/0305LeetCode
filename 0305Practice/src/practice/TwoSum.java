90fbb67144ef25451a4ab52824b1c5838829d886
joiechen <joie.chen@vechain.com>
2020-03-09 14:04:25 +0800

    day5

---
+++ 0305Practice/src/practice/TwoSum.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+  public static int[] twoSum01(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        map.put(nums[i], i);
+    }
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement) && map.get(complement) != i) {
+            return new int[] { i, map.get(complement) };
+        }
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
+
+  public static void main(String[] args) {
+    int[] test01 = {2, 5, 6, 8, 10};
+    int[] result = twoSum01(test01,10);
+    for(int i=0; i<result.length; i++){
+       System.out.println(result[i]);
+    }
+
+  }
+}
