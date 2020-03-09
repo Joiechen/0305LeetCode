5e8b2271e1b4a3acc49fc374ebcbb0a5da71c342
joiechen <joie.chen@vechain.com>
2020-03-09 14:05:56 +0800

    day5

--- 0305Practice/src/practice/TwoSum.java
+++ 0305Practice/src/practice/TwoSum.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class TwoSum {
 
+  //时间和空间复杂度O(n),哈希表的时间复杂度O(1) 大大简化了运算时间
   public static int[] twoSum01(int[] nums, int target) {
     Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
